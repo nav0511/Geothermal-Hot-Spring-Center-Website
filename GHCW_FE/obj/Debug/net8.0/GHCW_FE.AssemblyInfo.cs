56cced36b249b63dc49683b210b576bed67c9acb
Thiều Thành <110987144+JapanCitadel@users.noreply.github.com>
2024-10-12 18:22:51 +0700

    Chia lại folder

--- GHCW_FE/obj/Debug/net8.0/GHCW_FE.AssemblyInfo.cs
+++ GHCW_FE/obj/Debug/net8.0/GHCW_FE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GHCW_FE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103d4a098b430f775cefc18bf4c1278130fa001c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+463482ec8a320ae3de252d7024838ea924ed607b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GHCW_FE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GHCW_FE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
