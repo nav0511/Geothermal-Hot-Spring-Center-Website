220e8380ddc9bd76ab1f54eb74cb2869a6c31cbe
Thiều Thành <110987144+JapanCitadel@users.noreply.github.com>
2024-10-12 15:17:41 +0700

    pull main

--- GHCW_BE/obj/Debug/net8.0/GHCW_BE.AssemblyInfo.cs
+++ GHCW_BE/obj/Debug/net8.0/GHCW_BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GHCW_BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97a3167ea0a9dec2f7984162206b8c3423ef4cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eee4dd0a7e3682f757178274fe4d8258a05c929")]
 [assembly: System.Reflection.AssemblyProductAttribute("GHCW_BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GHCW_BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
