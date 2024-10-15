6a970b9cb5d41a2c42b54aabf55daf3e54e283cc
Thiều Thành <110987144+JapanCitadel@users.noreply.github.com>
2024-10-15 16:56:40 +0700

    hoàn thiện màn login, register, forgot password

--- GHCW_FE/obj/Debug/net8.0/GHCW_FE.AssemblyInfo.cs
+++ GHCW_FE/obj/Debug/net8.0/GHCW_FE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GHCW_FE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+463482ec8a320ae3de252d7024838ea924ed607b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56cced36b249b63dc49683b210b576bed67c9acb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GHCW_FE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GHCW_FE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
