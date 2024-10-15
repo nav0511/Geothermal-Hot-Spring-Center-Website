0f69e922cd4f1cee9d7709dd6ffa498e9af29486
nav0511 <133015300+nav0511@users.noreply.github.com>
2024-10-15 16:48:42 +0700

    list + detail pages

--- GHCW_FE/obj/Debug/net8.0/GHCW_FE.AssemblyInfo.cs
+++ GHCW_FE/obj/Debug/net8.0/GHCW_FE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GHCW_FE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103d4a098b430f775cefc18bf4c1278130fa001c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9fe75a38cade9d4559689b7f9a28574c92a3b3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GHCW_FE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GHCW_FE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
