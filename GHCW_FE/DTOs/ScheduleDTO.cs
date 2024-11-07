b2e6588d8d5208f758eecefacb37c9fbff91d58a
Thiều Thành <110987144+JapanCitadel@users.noreply.github.com>
2024-11-08 04:25:11 +0700

    Thành công list Schedule

--- GHCW_FE/DTOs/ScheduleDTO.cs
+++ GHCW_FE/DTOs/ScheduleDTO.cs
@@ -4,8 +4,15 @@
     {
         public int Id { get; set; }
         public int ReceptionistId { get; set; }
+        public string ReceptionistName { get; set; }
         public byte Shift { get; set; }
         public DateTime Date { get; set; }
         public bool IsActive { get; set; }
+    }
+
+    public class ScheduleByWeek
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
     }
 }
