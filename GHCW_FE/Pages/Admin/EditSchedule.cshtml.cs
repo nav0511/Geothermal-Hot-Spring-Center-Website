2dae041eb5427de59ca9d91bb727e5b276586856
Thiều Thành <110987144+JapanCitadel@users.noreply.github.com>
2024-11-09 04:19:08 +0700

    Hoàn thiện schedule management, tạo thêm customer management và employemanagement

---
+++ GHCW_FE/Pages/Admin/EditSchedule.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GHCW_FE.Pages.Admin
+{
+    public class EditScheduleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
