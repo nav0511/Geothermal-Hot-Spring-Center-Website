a66b4f091d134856a7569f5c2a5ed0ca8abb924c
NTP <phongdb05@gmail.com>
2024-11-12 21:51:01 +0700

    Update Css

--- GHCW_BE/DTOs/ProductDTO.cs
+++ GHCW_BE/DTOs/ProductDTO.cs
@@ -1,29 +1,67 @@
-﻿namespace GHCW_BE.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GHCW_BE.DTOs
 {
     public class ProductDTO
     {
+        [Required(ErrorMessage = "Yêu cầu phải có Id.")]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có mã danh mục.")]
         public int? CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có tên sản phẩm.")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có kích thước.")]
         public string? Size { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có mô tả.")]
         public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có giá.")]
         public double? Price { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có trạng thái thuê.")]
         public bool? IsForRent { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có hình ảnh.")]
         public string? Image { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có số lượng.")]
         public int? Quantity { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có trạng thái sẵn có.")]
         public bool? IsAvailable { get; set; }
     }
 
     public class ProductDTO2
     {
+        [Required(ErrorMessage = "Yêu cầu phải có mã danh mục.")]
         public int? CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có tên sản phẩm.")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có kích thước.")]
         public string? Size { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có mô tả.")]
         public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có giá.")]
         public double? Price { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có trạng thái thuê.")]
         public bool? IsForRent { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có hình ảnh.")]
         public string? Image { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có số lượng.")]
         public int? Quantity { get; set; }
+
+        [Required(ErrorMessage = "Yêu cầu phải có trạng thái sẵn có.")]
         public bool? IsAvailable { get; set; }
 
     }
